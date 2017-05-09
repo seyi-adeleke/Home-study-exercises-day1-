199da87b00077dba0addc9b788b5cc26dd3a9943
Seyi Adeleke <seyiadeleke42@yahoo.com>
2017-05-09 14:47:54 +0100

    created dataTypes branch

---
+++ dataTypes/spec/dataTypes-spec.js
@@ -0,0 +1,86 @@
+const data = require("../dataTypes");
+
+describe("Data types tests ", function() {
+    describe("Case for some falsy values", function() {
+
+        it("should return 'no value' for null", function() {
+            expect(data.dataTypes(null)).toEqual('no value');
+        });
+
+        it("should return 'no value' for undefined", function() {
+            expect(data.dataTypes(undefined)).toEqual('no value');
+        });
+
+    });
+
+    describe("Case for booleans", function() {
+
+        it("should return true for `true`", function() {
+            expect(data.dataTypes(true)).toBe(true);
+        });
+
+        it("should return false for `false`", function() {
+            expect(data.dataTypes(false)).toBe(false);
+        });
+
+    });
+
+    describe("Case for Numbers", function() {
+
+        it("should return 'less than 100' for 44", function() {
+            expect(data.dataTypes(44)).toBe('less than 100');
+        });
+
+        it("should return 'more than 100' for 144", function() {
+            expect(data.dataTypes(144)).toBe('more than 100');
+        });
+
+        it("should return 'equal to 100' for 100", function() {
+            expect(data.dataTypes(100)).toBe('equal to 100');
+        });
+
+
+    });
+
+    describe("Case for Strings", function() {
+
+        it("should return the length of `tergiversate`", function() {
+            expect(data.dataTypes('tergiversate')).toBe(12);
+        });
+
+        it("should return the length of an empty string", function() {
+            expect(data.dataTypes('')).toBe(0);
+        });
+
+        it("should return the length of `555`", function() {
+            expect(data.dataTypes('555')).toBe(3);
+        });
+
+    });
+
+    describe("Case for arrays", function() {
+
+        it("should return `2` for `[0, 1, 2]`", function() {
+            expect(data.dataTypes([0, 1, 2])).toBe(2);
+        });
+
+        it("should return `undefined` for `[]`", function() {
+            expect(data.dataTypes([])).not.toBeDefined();
+        });
+        it("should return `undefined` for `[4, 9]`", function() {
+            expect(data.dataTypes([4, 9])).not.toBeDefined();
+        });
+    });
+
+    describe("Case for functions", function() {
+        it("should call the `callback` function with argument true, and return `called callback`", function() {
+            var callback = function(arg) {
+                expect(arg).toBeTruthy();
+                if(arg === true) {
+                    return 'called callback';
+                }
+            };
+            expect(data.dataTypes(callback)).toBe('called callback');
+        });
+    });
+});
